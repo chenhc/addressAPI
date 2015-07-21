b36c9d76195e26122c9b76244f0ad46de1f9295e
chenhc <1031623808@qq.com>
2015-07-21 09:39:41 +0800

    Json接口增加至district

--- addressJsonAPI.php
+++ addressJsonAPI.php
@@ -13,20 +13,70 @@
 
      $sql = "select * from country where id =1";
      $result = mysql_query($sql);
-     while ($country = mysql_fetch_array($result,MYSQL_ASSOC)) 
+     $country = ''; 
+     while ($country_array = mysql_fetch_array($result,MYSQL_ASSOC)) 
      {
-          $sql = "select * from province where country_id =  '" . $country['id']. "' ";
-          $result = mysql_query($sql);
-          $prov_array = array();
-          while ($province = mysql_fetch_array($result,MYSQL_ASSOC)) 
-          {
-               $prov_array[] = $province;  
-          }
-          $country['provinces'] = $prov_array;
-          var_dump($country);
-          //print(json_encode($country));
-          echo "\n";
+        $country = $country_array;         
      }
+
+     $sql = "select * from province where country_id =  '" . $country['id']. "' ";
+     $result = mysql_query($sql);
+     $prov_array = array();
+      while ($province = mysql_fetch_array($result,MYSQL_ASSOC)) 
+      {
+        $prov_array[ $province['id'] ] = $province;  
+      }
+      $country['provinces'] = $prov_array;
+
+      
+      foreach ($country['provinces'] as $province) 
+      {
+        $province_id = $province['id'];
+        $sql = "select * from city where province_id =  '" . $province_id. "' ";
+        $result = mysql_query($sql);
+        $city_array =array();
+        while ($city = mysql_fetch_array($result,MYSQL_ASSOC)) 
+        {
+          $city_array[ $city['id']] = $city; 
+        }
+        $country['provinces'][$province_id]['cities'] = $city_array; 
+    
+      }
+
+      foreach ($country['provinces'] as $province) 
+      {
+          $province_id = $province['id'];
+
+          $sql = "select * from city where province_id =  '" . $province_id. "' ";
+          $result = mysql_query($sql);
+          $city_array =array();
+          while ($city = mysql_fetch_array($result,MYSQL_ASSOC)) 
+          {
+              $city_id = $city['id'];
+
+              $sql = "select * from district where city_id= '" . $city_id. "'";
+              $dist_result = mysql_query($sql);
+              $dist_array = array();
+              while ($district = mysql_fetch_array($dist_result,MYSQL_ASSOC)  ) 
+              {
+                  $district_id =$district['id'];
+                  $dist_array[$district_id] = $district;
+              }
+
+              $city['districts'] = $dist_array;
+              $city_array[$city_id] = $city;
+          }
+
+          $country['provinces'][$province_id]['cities'] = $city_array; 
+        
+      }
+      var_dump( $country['provinces'][9]['cities'][224]['districts'][2112]);
+      //var_dump($country);
+      
+      echo "\n";
+      print(json_encode($country['provinces'][9]['cities'][224]['districts'][2112]));
+      echo "\n";
+
 
     mysql_close($conn);  
 ?>
